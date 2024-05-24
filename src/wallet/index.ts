70d353c27ac3cb6d04dde7ba40b444443609c0ab
BHouwens <zhion360@gmail.com>
2024-05-24 09:42:47 +0200

    Adding to key-handling wallet

--- src/wallet/index.ts
+++ src/wallet/index.ts
@@ -2,6 +2,7 @@ import { Wallet } from '@2waychain/2wayjs';
 
 export default class KeyWallet {
   private wallet: Wallet;
+  private cacheKeys: Map<Uint8Array, Uint8Array>;
   private CONFIG = {
     mempoolHost: 'https://mempool.aiblock.ch',
     passphrase: '2waychain',
@@ -9,14 +10,44 @@ export default class KeyWallet {
   
   constructor() {
     this.wallet = new Wallet();
+    this.cacheKeys = new Map();
   }
 
   async init(seedPhrase: string) {
     await this.wallet.fromSeed(seedPhrase, this.CONFIG);
   }
 
-  providePublicKey() {
-    
+  generateKeypair() {
+      const encryptedKeypairResp = this.wallet.getNewKeypair([]);
+
+      if (encryptedKeypairResp.content) {
+        const encryptedKeypair = encryptedKeypairResp.content.newKeypairResponse;
+
+        if (encryptedKeypair) {
+          const decryption = this.wallet.decryptKeypair(encryptedKeypair);
+          return decryption.content?.decryptKeypairResponse;
+
+        }
+      }
+
+      return null;
+  }
+
+  providePublicKey(): Uint8Array | null {
+    // See if the cacheKeys has a public key
+    if (this.cacheKeys.size > 0) {
+      // Return the first public key
+      return this.cacheKeys.keys().next().value;
+    }
+
+    // Generate a new keypair
+    const keyPair = this.generateKeypair();
+    if (keyPair) {
+      this.cacheKeys.set(keyPair.publicKey, keyPair.secretKey);
+      return keyPair.publicKey;
+    }
+
+    return null;
   }
 
   encryptWithPublicKey(publicKey: string, message: string) {
